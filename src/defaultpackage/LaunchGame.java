94a932402ee686e94ec097a1d63571ae923d3c0f
Pragya <pragya4351@gmail.com>
2024-08-27 01:33:05 +0530

    Part 1 complete

--- src/defaultpackage/LaunchGame.java
+++ src/defaultpackage/LaunchGame.java
@@ -35,6 +35,52 @@ class TicTacToe
             System.out.println("-------------");
         }
     }
+    
+    void placemark(int row, int col, char mark)
+    {
+    	if(row >= 0 && row <=2 && col >= 0 && col<=2)
+    	{
+    		board[row][col] = mark;
+    	}
+    	else
+    	{
+    		System.out.println("Invalid position");
+    	}
+    }
+    
+    boolean checklColWin()
+    {
+    	for(int j=0; j<=2; j++)
+    	{
+    		if(board[0][j] == board[1][j] && board[1][j] == board[2][j])
+    		{
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    boolean checkRowWin()
+    {
+    	for(int i=0; i<=2; i++)
+    	{
+    		if(board[i][0] == board[i][1] && board[i][1] == board[i][2])
+    		{
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+    
+    boolean checkDiagWin()
+    {
+    	if(board[0][0] == board[1][1] && board[1][1] == board[2][2] || 
+    			board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    	{
+    		return true;
+    	}
+    	return false;
+    }
 }
 
 public class LaunchGame {
@@ -42,6 +88,11 @@ public class LaunchGame {
 	public static void main(String[] args) {
 		TicTacToe t = new TicTacToe();
 		t.dispBoard();
+		t.placemark(0, 2, 'X');
+		t.placemark(1, 1, 'X');
+		t.placemark(2, 0, 'X');
+		t.dispBoard();
+		System.out.println(t.checkDiagWin());
 	}
 
 }
