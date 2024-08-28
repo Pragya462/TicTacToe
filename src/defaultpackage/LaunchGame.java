ff652930bb41ff177c34036e19352761cb704134
Pragya <pragya4351@gmail.com>
2024-08-29 01:39:26 +0530

    Game Complete

--- src/defaultpackage/LaunchGame.java
+++ src/defaultpackage/LaunchGame.java
@@ -1,8 +1,11 @@
 package defaultpackage;
 
+import java.util.Random;
+import java.util.Scanner;
+
 class TicTacToe
 {
-    char[][] board;
+    static char[][] board;
     
     public TicTacToe()
     {
@@ -21,7 +24,7 @@ class TicTacToe
         }
     }
     
-    void dispBoard()
+    static void dispBoard()
     {
     	System.out.println("-------------");
     	for(int i=0; i<board.length; i++)
@@ -36,7 +39,7 @@ class TicTacToe
         }
     }
     
-    void placemark(int row, int col, char mark)
+    static void placemark(int row, int col, char mark)
     {
     	if(row >= 0 && row <=2 && col >= 0 && col<=2)
     	{
@@ -48,11 +51,11 @@ class TicTacToe
     	}
     }
     
-    boolean checklColWin()
+    static boolean checkColWin()
     {
     	for(int j=0; j<=2; j++)
     	{
-    		if(board[0][j] == board[1][j] && board[1][j] == board[2][j])
+    		if(board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][j] == board[2][j])
     		{
     			return true;
     		}
@@ -60,11 +63,11 @@ class TicTacToe
     	return false;
     }
     
-    boolean checkRowWin()
+    static boolean checkRowWin()
     {
     	for(int i=0; i<=2; i++)
     	{
-    		if(board[i][0] == board[i][1] && board[i][1] == board[i][2])
+    		if(board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
     		{
     			return true;
     		}
@@ -72,27 +75,138 @@ class TicTacToe
     	return false;
     }
     
-    boolean checkDiagWin()
+    static boolean checkDiagWin()
     {
-    	if(board[0][0] == board[1][1] && board[1][1] == board[2][2] || 
-    			board[0][2] == board[1][1] && board[1][1] == board[2][0])
+    	if(board[0][0] != ' ' &&  board[0][0] == board[1][1] && board[1][1] == board[2][2] || 
+    			board[0][2] != ' ' &&  board[0][2] == board[1][1] && board[1][1] == board[2][0])
     	{
     		return true;
     	}
     	return false;
     }
+    
+    static boolean checkDraw()
+    {
+    	for(int i=0; i<=2; i++)
+    	{
+    		for(int j=0; j<=2; j++)
+    		{
+    			if(board[i][j] == ' ')
+    			{
+    				return false;
+    			}
+    		}
+    	}
+    	return true;
+    }
+}
+
+abstract class Player
+{
+	String name;
+	char mark;
+	
+	abstract void makeMove();
+	
+	boolean isValidMove(int row, int col)
+	{
+		if(row>=0 && row<=2 && col >=0 && col<=2)
+		{
+			if(TicTacToe.board[row][col] == ' ')
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+class HumanPlayer extends Player
+{
+	HumanPlayer(String name, char mark)
+	{
+		this.name = name;
+		this.mark = mark;
+	}
+	
+	void makeMove()
+	{
+		Scanner scan = new Scanner(System.in);
+		int row, col;
+		do {
+			System.out.println("Enter the row and col");
+			row= scan.nextInt();
+			col= scan.nextInt();
+			
+		}while(!isValidMove(row, col));
+		
+		TicTacToe.placemark(row, col, mark);
+	}
+}
+
+class AIPlayer extends Player
+{
+	String name;
+	char mark;
+	
+	AIPlayer(String name, char mark)
+	{
+		this.name = name;
+		this.mark = mark;
+	}
+	
+	void makeMove()
+	{
+		Scanner scan = new Scanner(System.in);
+		int row, col;
+		do {
+			Random r = new Random();
+			row = r.nextInt(3);
+			col = r.nextInt(3);
+		}while(!isValidMove(row, col));
+		
+		TicTacToe.placemark(row, col, mark);
+	}
 }
 
 public class LaunchGame {
 
 	public static void main(String[] args) {
 		TicTacToe t = new TicTacToe();
-		t.dispBoard();
-		t.placemark(0, 2, 'X');
-		t.placemark(1, 1, 'X');
-		t.placemark(2, 0, 'X');
-		t.dispBoard();
-		System.out.println(t.checkDiagWin());
+		
+		HumanPlayer p1 = new HumanPlayer("Bob", 'X');
+		AIPlayer p2 = new AIPlayer("AI", 'O');
+		
+		Player cp;
+		cp=p1;
+		
+		do
+		{
+			System.out.println(cp.name+" turn");
+			cp.makeMove();
+			TicTacToe.dispBoard();
+			if(TicTacToe.checkColWin() || TicTacToe.checkRowWin() || TicTacToe.checkDiagWin())
+			{
+				System.out.println(cp.name+" has won");
+				break;
+			}
+			else if(TicTacToe.checkDraw())
+			{
+				System.out.println("Game is a draw");
+				break;
+			}
+			else
+			{
+				if(cp==p1)
+				{
+					cp=p2;
+				}
+				else
+				{
+					cp=p1;
+				}
+			}
+		}while(true);
 	}
 
 }
